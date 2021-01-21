4073cfc757b4a8243516044a603b0a7945fcd45f
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-21 17:02:09 -0500

    commit tree b7db573ade682cc035f34c66333afd2f426310fa

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,11 @@ require_relative '../lib/person'
 
 RSpec.configure do |config|
   # config here
+  class Person
+    def initialize(attributes)
+     attributes.each do |key, value|
+       self.classattr_accessor(key)
+       self.send(("#{key}="), value)
+    end 
+  end
 end
